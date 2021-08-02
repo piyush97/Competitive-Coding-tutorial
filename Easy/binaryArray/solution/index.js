4ab6216aeb522b71168fb269d22610b51eb9d5a7
Piyush Mehta <me@piyushmehta.com>
2021-08-02 17:23:32 +0530

    Binary Array

---
+++ Easy/binaryArray/solution/index.js
@@ -0,0 +1,32 @@
+/**
+ * Brute Force method to sort a binary array in linear time
+ * Time Complexity O(n)
+ * @param {[]} array
+ */
+const sort = (array) => {
+  // count number of 0's
+  let zeros = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == 0) {
+      zeros++;
+    }
+  }
+
+  // put 0's at the beginning
+  // Can be replaced with a for loop too make
+  // sure to keep the idex global for solving in linear time
+  let k = 0;
+  while (zeros--) {
+    array[k++] = 0;
+  }
+
+  // fill all remaining elements by 1
+  while (k < array.length) {
+    array[k++] = 1;
+  }
+  console.log(array);
+};
+
+console.time("Time Taken by BruteForce");
+sort([0, 0, 1, 0, 1, 1, 0, 1, 0, 0]);
+console.timeEnd("Time Taken by BruteForce");
