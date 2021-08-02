615bd74d62d8770b9a330440eb07d7a059fdaebb
Piyush Mehta <me@piyushmehta.com>
2021-08-02 18:43:44 +0530

    Let the devil cry

--- Easy/binaryArray/solution/index.js
+++ Easy/binaryArray/solution/index.js
@@ -26,7 +26,3 @@ export const sort = (array) => {
   }
   return array;
 };
-
-console.time("Time Taken by BruteForce");
-sort([0, 0, 1, 0, 1, 1, 0, 1, 0, 0]);
-console.timeEnd("Time Taken by BruteForce");
